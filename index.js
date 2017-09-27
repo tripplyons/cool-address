cb31239052176de39e98b697dbf00d5118debd78
Tripp Lyons <tripplyons@gmail.com>
2017-09-26 20:03:15 -0700

    Calculate private key after finding match

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ var bitcoin = require('bitcoinjs-lib')
 var words = fs.readFileSync('words.txt', 'utf8').split('\n')
 
 function generateAddress() {
-	var keyPair = bitcoin.ECPair.makeRandom()
-	return {
-		public: keyPair.getAddress(),
-		private: keyPair.toWIF()
-	}
+	return bitcoin.ECPair.makeRandom()
 }
 
 for(var i=0; i<10000; i++) {
 	var current = generateAddress()
-	var lower = current.public.toLowerCase().substr(1)
+	var lower = current.getAddress().toLowerCase().substr(1)
 	var matchedWords = words.filter(function(word) {
 	 	return lower.startsWith(word)
 	})
 	if(matchedWords[0]) {
-		console.log(current.public + ' - ' + current.private)
+		console.log(current.getAddress() + ' - ' + current.toWIF())
 	}
 }
